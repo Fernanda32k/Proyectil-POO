f8514ab6d5c2d02bae906e183317b2e90879f4c7
juan2007moran00-source <juan2007moran00@gmail.com>
2025-09-23 18:31:19 -0500

    Update Proyectil.java

--- Proyectil.java
+++ Proyectil.java
@@ -1,9 +1,26 @@
 public class Proyectil{
   protected double masa;
   protected double v0;
-  protected double phi;
-  protected double tetha;
+  protected double phi;  // angulo Horizontal
+  protected double tetha; // angulo vertical
+  protected double pos0_x;
+  protected double pos0_y;
+  protected double pos0_z;
+  protected double v0_x;
+  protected double v0_y;
+  protected double v0_z;
+
+
+  protected double calcularV0EjeX{
+     return v0 * Math.cos(theta) * Math.cos(phi);
+  }
+
+  protected double calularVOEjeY{
+    return v0 * Math.cos(theta)* Marh.sin(phi);
+  }
   
-  
+  protected double calularVOEjeZ{
+    return v0 * Marh.sin(phi);
+  }
   
 }
