a53325f8331a5fefb36e36462e71892202cc7290
Orteqk <alejo.orte99@gmail.com>
2025-09-24 17:30:44 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,23 +1,13 @@
 // Ejemplo de uso
 public class Main {
     public static void main(String[] args) {
-        /*PlanoCartesiano plano = new PlanoCartesiano();
-
-        // Simular trayectoria de proyectil
-        for (double t = 0; t <= 5; t += 0.05) {
-            double x = 2 * t;
-            double y = 5 * t - 1 * t * t; // parábola
-            if (y < 0) break;
-            plano.marcarPunto(x, y);
-        }
-
-        plano.imprimir(); */
 
 
-        Proyectil proyectil = new Proyectil();
-        CalculosSinResistencia sr = new CalculosSinResistencia(proyectil);
-        sr.calcularTrayectoria();
-        sr.mostrarTrayectoria();
+        Proyectil proyectil = new Proyectil(); //Solicita datos al usuario y calcula los restantes vo_x, vo_y vo_z
+        CalculosSinResistencia sr = new CalculosSinResistencia(proyectil); //Instance objeto y inicializa tableros y atributos
+        sr.calcularTrayectoria();   //Hace los calculos con las formulas
+        sr.mostrarTrayectoria();   // Muestra los resultado hasta que encuentra un valor negativo
 
     }
+
 }
